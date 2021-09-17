2e486059fdfafc5847c1937c3984fbf41d791859
Niclas van Eyk <niclas.eyk@gmail.com>
2021-09-17 13:02:17 +0200

    WIP

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        \App\AppServiceProvider::class,
         /*
          * Domain Service Providers...
          */
