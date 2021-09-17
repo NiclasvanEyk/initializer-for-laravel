5b0e5466648324663b4e5e3fbdc447ba8a9dd90d
Niclas van Eyk <niclas.eyk@gmail.com>
2021-09-17 13:02:13 +0200

    WIP

---
+++ app/AppServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+}
