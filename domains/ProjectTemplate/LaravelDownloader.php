40c83360a1547a4a555be6364b381c26471b5aea
Niclas van Eyk <niclas.eyk@gmail.com>
2021-09-20 14:15:28 +0200

    normalize downloads

--- domains/ProjectTemplate/LaravelDownloader.php
+++ domains/ProjectTemplate/LaravelDownloader.php
@@ -4,8 +4,11 @@ namespace Domains\ProjectTemplate;
 
 use Domains\Packagist\Models\Package;
 use Domains\Packagist\PackagistApiClient;
+use Domains\Support\FileSystem\Path;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use League\Flysystem\Adapter\Local;
 use PhpZip\ZipFile;
 
 class LaravelDownloader
@@ -39,15 +42,36 @@ class LaravelDownloader
     {
         $response = Http::get($package->dist->url)->body();
 
-        Log::info("Downloading $package->version...");
+        Log::info("Downloading $package->version from {$package->dist->url}...");
+
+        // The archives contain a folder called `laravel-laravel-somehash`,
+        // so we need to extract and re-zip the contents of that folder
+        $downloadedArchive = (new ZipFile())->openFromString($response);
 
         $release = new DownloadedLaravelRelease(
             package: $package,
-            archive: (new ZipFile())->openFromString($response),
+            archive: $this->normalizedArchive($downloadedArchive),
         );
 
         Log::info("$package->version successfully downloaded!");
 
         return $release;
+    }
+
+    private function normalizedArchive(ZipFile $downloadedArchive): ZipFile
+    {
+        $temp = (new Local(Path::join(
+            sys_get_temp_dir(),
+            'initializer-for-laravel',
+            'downloaded-templates',
+            Str::uuid(),
+        )));
+
+        $downloadedArchive->extractTo($temp->getPathPrefix());
+        $folderName = $downloadedArchive->getListFiles()[0];
+
+        return (new ZipFile())->addDirRecursive(
+            $temp->applyPathPrefix($folderName)
+        );
     }
 }
