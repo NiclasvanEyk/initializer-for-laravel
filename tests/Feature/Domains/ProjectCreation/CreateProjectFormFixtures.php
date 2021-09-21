65ad15b42f52034919e914fb87e2c2ebca6ba956
Niclas van Eyk <niclas.eyk@gmail.com>
2021-09-21 20:39:11 +0200

    cover validation

--- tests/Feature/Domains/ProjectCreation/CreateProjectFormFixtures.php
+++ tests/Feature/Domains/ProjectCreation/CreateProjectFormFixtures.php
@@ -3,25 +3,86 @@
 namespace Tests\Feature\Domains\ProjectCreation;
 
 use Domains\CreateProjectForm\CreateProjectForm;
+use Domains\CreateProjectForm\Http\Request\CreateProjectRequest\CreateProjectRequestParameter as P;
 use Domains\CreateProjectForm\Sections\Authentication;
 use Domains\CreateProjectForm\Sections\Cache;
+use Domains\CreateProjectForm\Sections\Cache\CacheOption;
 use Domains\CreateProjectForm\Sections\Cache\RedisCacheDriver;
+use Domains\CreateProjectForm\Sections\Cashier\CashierDriverOption;
 use Domains\CreateProjectForm\Sections\Cashier\CashierStripeDriver;
 use Domains\CreateProjectForm\Sections\Database;
+use Domains\CreateProjectForm\Sections\Database\DatabaseOption;
 use Domains\CreateProjectForm\Sections\DevelopmentTools;
 use Domains\CreateProjectForm\Sections\Metadata;
+use Domains\CreateProjectForm\Sections\Metadata\PhpVersion;
 use Domains\CreateProjectForm\Sections\Payment;
 use Domains\CreateProjectForm\Sections\Queue;
+use Domains\CreateProjectForm\Sections\Queue\QueueDriverOption;
 use Domains\CreateProjectForm\Sections\Queue\RedisQueueDriver;
 use Domains\CreateProjectForm\Sections\Scout\MeiliSearchScoutDriver;
+use Domains\CreateProjectForm\Sections\Scout\ScoutDriverOption;
 use Domains\CreateProjectForm\Sections\Search;
 use Domains\CreateProjectForm\Sections\Storage;
 use Domains\CreateProjectForm\Sections\Testing;
 use Domains\Laravel\Sail\MySQLDatabase;
+use Domains\Laravel\StarterKit\BreezeFrontend;
 use Domains\Laravel\StarterKit\Laravel;
+use Domains\Laravel\StarterKit\StarterKit;
 
 class CreateProjectFormFixtures
 {
+    public static function allParameters()
+    {
+        return [
+            /** @see Metadata */
+            P::VENDOR => 'foo',
+            P::PROJECT => 'bar',
+            P::PHP => PhpVersion::v8_0,
+            P::DESCRIPTION => '',
+
+            /** @see Authentication */
+            P::STARTER => StarterKit::BREEZE,
+            // Does not make sense here, we use breeze
+//            P::USES_JETSTREAM_TEAMS => '',
+//            P::JETSTREAM_FRONTEND => '',
+            P::BREEZE_FRONTEND => BreezeFrontend::BLADE,
+            P::USES_FORTIFY => true,
+            P::USES_PASSPORT => true,
+            P::USES_SOCIALITE => true,
+
+            /** @see Database */
+            P::DATABASE => DatabaseOption::default(),
+
+            /** @see Cache */
+            P::CACHE_DRIVER => CacheOption::default(),
+
+            /** @see Search */
+            P::SCOUT_DRIVER => ScoutDriverOption::default(),
+
+            /** @see Queue */
+            P::QUEUE_DRIVER => QueueDriverOption::default(),
+            P::USES_HORIZON => true,
+
+            /** @see DevelopmentTools */
+            P::USES_TELESCOPE => true,
+            P::USES_MAILHOG => true,
+            P::USES_ENVOY => true,
+
+            /** @see Testing */
+            P::USES_DUSK => true,
+            P::USES_PEST => true,
+
+            /** @see Payment */
+            P::CASHIER_DRIVER => CashierDriverOption::STRIPE,
+
+            /** @see Storage */
+            P::USES_MINIO => true,
+            P::USES_FLYSYSTEM_S3_DRIVER => true,
+            P::USES_FLYSYSTEM_SFTP_DRIVER => true,
+            P::USES_FLYSYSTEM_CACHED_ADAPTER => true,
+        ];
+    }
+
     public static function allOptionsEnabled(
         ?Metadata         $metadata = null,
         ?Authentication   $authentication = null,
