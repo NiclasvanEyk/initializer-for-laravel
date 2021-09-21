7da5204f484b0906d74881723e99586729a12706
Jonas Uekötter <ueman@users.noreply.github.com>
2021-09-21 09:19:16 +0200

    Improve wording

--- domains/ProjectTemplateCustomization/resources/templates/initialize.blade.php
+++ domains/ProjectTemplateCustomization/resources/templates/initialize.blade.php
@@ -37,7 +37,7 @@ rm "./{{ $initializationScript }}";
 echo '';
 <x-shell::banner title="Done!">
 <x-shell::banner-line />
-<x-shell::banner-line>You can now have a look at README.md, for further instructions, guides</x-shell::banner-line>
+<x-shell::banner-line>Now you can take a look at the README.md, for further instructions, guides</x-shell::banner-line>
 <x-shell::banner-line>and links to the installed components.</x-shell::banner-line>
 <x-shell::banner-line />
 <x-shell::banner-line>Some helpful links:</x-shell::banner-line>
