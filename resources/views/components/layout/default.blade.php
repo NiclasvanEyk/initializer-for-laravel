3cd2d36694a0c336bb91bfc199f8adfbfb5c84a8
Niclas van Eyk <niclas.eyk@gmail.com>
2021-09-22 20:05:58 +0200

    og

--- resources/views/components/layout/default.blade.php
+++ resources/views/components/layout/default.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="{{ config('app.name') }}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ url('/') }}" />
+    <meta property="og:image" content="{{ url('/img/og/initializer-for-laravel.png') }}" />
 
     <title>{{ config('app.name') }}</title>
 
