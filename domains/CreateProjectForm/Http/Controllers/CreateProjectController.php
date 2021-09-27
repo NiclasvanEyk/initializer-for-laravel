cca4300697f3bc4dfb635ededd9a6977af934d7a
Niclas van Eyk <niclas.eyk@gmail.com>
2021-09-27 13:53:55 +0200

    Logging

--- domains/CreateProjectForm/Http/Controllers/CreateProjectController.php
+++ domains/CreateProjectForm/Http/Controllers/CreateProjectController.php
@@ -4,6 +4,7 @@ namespace Domains\CreateProjectForm\Http\Controllers;
 
 use Domains\CreateProjectForm\Http\Request\CreateProjectRequest;
 use Domains\ProjectTemplateCustomization\ProjectTemplateCustomizer;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CreateProjectController
@@ -15,6 +16,11 @@ class CreateProjectController
         $form = $request->buildForm();
         $name = $form->metadata->projectName;
         $archive = $builder->build($form);
+
+        Log::info("Initialized project!", [
+            'starter' => $form->authentication->starterKit->name,
+            'database' => $form->database->database->id(),
+        ]);
 
         return $archive->outputAsSymfonyResponse("$name.zip");
     }
