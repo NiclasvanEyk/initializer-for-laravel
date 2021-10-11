494daed1737cb4a211032813ba7ef9bb721b87f1
Niclas van Eyk <niclas.eyk@gmail.com>
2021-10-11 17:27:54 +0200

    Add google site verification

--- resources/views/components/layout/default.blade.php
+++ resources/views/components/layout/default.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="LtXTpnxyz3li3YHOVCUUgIM_pGUf9Da8QEk48LVqeck" />
 
     <x-head.opengraph-tags
         :url="url('/')"
