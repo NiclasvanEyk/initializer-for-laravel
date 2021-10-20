01fef96780b1c5052116a5389ad66efda3fdbd1d
Niclas van Eyk <niclas.eyk@gmail.com>
2021-10-20 19:10:02 +0200

    actually use version as runtime
    This is why 7.4 was removed earlier, as in my tests it
    did not install when using the 74 container. I also think
    every _new_ project should be started with 8 anyways, as
    the active support for 7.4 ends in 1 month

--- domains/PostDownload/Tasks/SetupSail.php
+++ domains/PostDownload/Tasks/SetupSail.php
@@ -2,10 +2,12 @@
 
 namespace Domains\PostDownload\Tasks;
 
+use Domains\CreateProjectForm\Sections\Metadata\PhpVersion;
 use Domains\Laravel\Sail\DatabaseOption;
 use Domains\Laravel\Sail\SailConfigurationOption;
 use Domains\PostDownload\PostDownloadTask;
 use Domains\PostDownload\PostDownloadTaskGroup;
+use Domains\SourceCodeManipulation\Perl\Perl;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
@@ -40,7 +42,13 @@ class SetupSail implements PostDownloadTaskGroup, PostDownloadTask
 
     private function phpContainer(): string
     {
-        $phpContainerVersion = Str::of($this->phpVersion)->remove('.');
+        // We want to use the 80 container for v8.1, as there is no 81 container
+        // published yet.
+        $containerVersion = $this->phpVersion === PhpVersion::v8_1
+            ? PhpVersion::v8_0
+            : $this->phpVersion;
+
+        $phpContainerVersion = Str::of($containerVersion)->remove('.');
 
         return "laravelsail/php$phpContainerVersion-composer:latest";
     }
@@ -59,10 +67,24 @@ class SetupSail implements PostDownloadTaskGroup, PostDownloadTask
     private function composerInstallCommand(): string
     {
         return join(' && ', [
+            // These steps are mostly the same as laravel.build does
             'composer install --ignore-platform-reqs',
             "php -r \\\"file_exists('.env') || copy('.env.example', '.env');\\\"",
             'php artisan key:generate --ansi',
+            // We explicitly select only the chosen sail services
             "php artisan sail:install --with={$this->sailServices()}",
+            // We also adjust the runtime here based on the chosen PHP version
+            Perl::replace(
+                file: 'docker-compose.yml',
+                pattern: '\.\/vendor\/laravel\/sail\/runtimes\/\d\.\d',
+                replacement: ".\/vendor\/laravel\/sail\/runtimes\/$this->phpVersion",
+            ),
+            // and adjust the image name to match the PHP version
+            Perl::replace(
+                file: 'docker-compose.yml',
+                pattern: 'sail-\d\.\d\/app',
+                replacement: "sail-$this->phpVersion\/app",
+            ),
         ]);
     }
 }
