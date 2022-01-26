995cecc05cc06efa26c6adde5f09ae1ce8820819
Niclas van Eyk <niclas.eyk@gmail.com>
2022-01-26 22:16:36 +0100

    Count used options and generated projects

--- domains/CreateProjectForm/Http/Controllers/CreateProjectController.php
+++ domains/CreateProjectForm/Http/Controllers/CreateProjectController.php
@@ -4,23 +4,23 @@ namespace Domains\CreateProjectForm\Http\Controllers;
 
 use Domains\CreateProjectForm\Http\Request\CreateProjectRequest;
 use Domains\ProjectTemplateCustomization\ProjectTemplateCustomizer;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Application;
 use Symfony\Component\HttpFoundation\Response;
-
+use Tests\Feature\Domains\Statistics\StatisticsService;
+ª
 class CreateProjectController
 {
     public function __invoke(
         CreateProjectRequest $request,
         ProjectTemplateCustomizer $builder,
+        Application $app,
+        StatisticsService $statistics,
     ): Response {
         $form = $request->buildForm();
         $name = $form->metadata->projectName;
         $archive = $builder->build($form);
 
-        Log::info('Initialized project!', [
-            'starter' => $form->authentication->starterKit->name,
-            'database' => $form->database->database->id(),
-        ]);
+        $app->terminating(fn () => rescue(fn () => $statistics->record($request)));
 
         return $archive->outputAsSymfonyResponse("$name.zip");
     }
