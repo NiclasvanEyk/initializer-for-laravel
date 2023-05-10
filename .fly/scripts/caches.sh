769fbb0ec653c3e6cd003b02cda4e88656b67164
Niclas van Eyk <niclas.eyk@gmail.com>
2023-05-10 23:24:28 +0200

    link storage on fly

--- .fly/scripts/caches.sh
+++ .fly/scripts/caches.sh
@@ -7,3 +7,4 @@
 /usr/bin/php /var/www/html/artisan migrate --force --no-ansi -q
 /usr/bin/php /var/www/html/artisan initializer:update-template --no-ansi -q
 /usr/bin/php /var/www/html/artisan google-fonts:fetch --no-ansi -q
+/usr/bin/php /var/www/html/artisan storage:link --no-ansi -q
