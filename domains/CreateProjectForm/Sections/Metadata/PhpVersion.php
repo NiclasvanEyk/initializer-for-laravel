7212ac63e0c694d8218f1bc4adf28553d3eee610
Niclas van Eyk <niclas.eyk@gmail.com>
2024-12-19 22:52:42 +0100

    use php8.4 by default

--- domains/CreateProjectForm/Sections/Metadata/PhpVersion.php
+++ domains/CreateProjectForm/Sections/Metadata/PhpVersion.php
@@ -11,10 +11,11 @@ class PhpVersion
     public const v8_1 = '8.1';
     public const v8_2 = '8.2';
     public const v8_3 = '8.3';
+    public const v8_4 = '8.4';
 
     public static function latest(): string
     {
-        return self::v8_3;
+        return self::v8_4;
     }
 
     public static function preview(): ?string
